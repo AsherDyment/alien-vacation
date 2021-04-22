1deced9595bcbe3215635a7f71330227542fa683
AsherDyment <77258372+AsherDyment@users.noreply.github.com>
2021-04-22 18:36:54 -0400

    a lot

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-
+info.setLife(3)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(sprite: Sprite, otherSprite: Sprite) {
+    
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+let timeintervul = 1000
 let ship = sprites.create(img`
     ........feebbbef........
     ........f24bdb2e........
@@ -21,7 +27,8 @@ ship.setPosition(15, 60)
 controller.moveSprite(ship, 0, 100)
 ship.setStayInScreen(true)
 
-game.onUpdateInterval(500, function() {
+
+game.onUpdateInterval(timeintervul, function() {
     let rock = sprites.create(img`
         . . . . . . . . b b b b b . . .
         . . . . . . b b d d d d b b . .
@@ -44,10 +51,32 @@ game.onUpdateInterval(500, function() {
     rock.vx = -50
 rock.setFlag(SpriteFlag.AutoDestroy, true)
 if (info.score()<= 10) {
-rock.vx = -40
+rock.vx = -65
+controller.moveSprite(ship, 0, 100)
 }
 else if (info.score() >=11 && info.score() <=20 ) {
-rock.vx = -60
+rock.vx = -80
+timeintervul = 800
+controller.moveSprite(ship, 0, 150)
+}
+else if (info.score() >=21 && info.score() <=30 ) {
+rock.vx = -100
+timeintervul = 300
+controller.moveSprite(ship, 0, 200)
+}
+else if (info.score() >=31 && info.score() <=40 ) {
+rock.vx = -120
+timeintervul = 200
+controller.moveSprite(ship, 0, 250)
+}
+else if (info.score() >=41 && info.score() <=50 ) {
+rock.vx = -130
+timeintervul = 100
+controller.moveSprite(ship, 0, 300)
+}
+else if (info.score() >=50 ) { rock.vx = -150
+controller.moveSprite(ship, 0, 350) 
+timeintervul = 50
 }
 })
 sprites.onDestroyed(SpriteKind.Enemy, function(sprite: Sprite) {
